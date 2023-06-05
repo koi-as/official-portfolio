54d4c5b7f4c763e5b2f88d2106fabaabd5d09d95
Koi-AS <lightningblade.901@gmail.com>
2023-06-05 19:59:42 -0400

    created a base app component to compile the other components together. Includes navbar and main

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Nav from './components/Nav';
+import Main from './components/Main';
+
+function App() {
+  return (
+  <div>
+    <Nav />
+    <Main />
+  </div>
+  );
+};
+
+export default App;
