2d4037d7b8bb3ae7d99f1a9830323688ec4dfcce
Koi-AS <lightningblade.901@gmail.com>
2023-06-08 16:55:44 -0400

    Added logic for switching pages and imported nav. This file renders the appropriately selected page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import AboutMe from './AboutMe';
-import ApplicationCard from './ApplicationCard';
+import Nav from './Nav';
+import AboutMe from './pages/AboutMe';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Repository from './pages/Repository';
 
-function Main () {
+export default function Main () {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  const renderPage = () => {
+    if (currentPage === 'Contact') {
+      return <Contact />
+    }
+    if (currentPage === 'AboutMe') {
+      return <AboutMe />
+    }
+    if (currentPage === 'Repository') {
+      return <Repository />
+    }
+    return <Home />
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
   return (
     <div>
-      <AboutMe />
-      <ApplicationCard />
+      <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
     </div>
   )
 };
-
-export default Main;
