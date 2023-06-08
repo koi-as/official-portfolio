1fc8b614186ae37fab36d9ef99324fd489908ccc
Koi-AS <lightningblade.901@gmail.com>
2023-06-08 16:57:51 -0400

    Added a home page. Just describes what to look for and where

---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className='mb-5'>Hello and welcome to my portfolio!</h1>
+      <p className='mb-2'>To take a look at some of my past work, click on the Repositories tab</p>
+      <p className='mb-2'>If you want to know more about me and my ambitions, click on the About me tab</p>
+      <p className='mb-2'>If you need to get in contact with me, click on the Contact Me tab</p>
+    </div>
+  )
+}
