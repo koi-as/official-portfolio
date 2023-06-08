d5a253033d5a3333ba4362a1c9938a3f8c635a28
Koi-AS <lightningblade.901@gmail.com>
2023-06-08 16:54:16 -0400

    Added logic to switch pages based on which tab was selected

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,39 @@
 import React from 'react';
 
-function Nav() {
+function Nav({ currentPage, handlePageChange }) {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <div className="navbar-brand">Koi | Alex Seidensticker</div>
+        <div className="navbar-brand">koi-as | Alex Seidensticker</div>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div className="navbar-nav">
-            <a className="nav-link active" href="https://github.com/koi-as">GitHub</a>
-            <a className="nav-link active" href="https://www.linkedin.com/in/alex-koi-seidensticker">LinkedIn</a>
-            <a className="nav-link active" href="">Email</a>
+            <a
+              className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+              onClick={() => handlePageChange('Home')}
+              href='#home'>
+              Home
+            </a>
+            <a
+              className={currentPage === 'AboutMe' ? 'nav-link active' : 'nav-link'}
+              onClick={() => handlePageChange('AboutMe')}
+              href="#aboutme">
+              About Me
+            </a>
+            <a
+              className={currentPage === 'Repository' ? 'nav-link active' : 'nav-link'}
+              onClick={() => handlePageChange('Repository')}
+              href="#repository">
+              Repositories
+            </a>
+            <a
+              className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+              onClick={() => handlePageChange('Contact')}
+              href="#contact">
+              Contact Me
+            </a>
           </div>
         </div>
       </div>
